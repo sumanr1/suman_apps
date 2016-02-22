74d1f56251f77b8a2342370bd332e72e76c93386
suman <suman@suman-OEM.(none)>
2016-02-22 19:24:03 +0530

    name changes

--- app_shop/app/controllers/home_controller.rb
+++ app_shop/app/controllers/home_controller.rb
@@ -7,9 +7,8 @@ class HomeController < AuthenticatedController
     #@products = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
   
   a = ShopifyAPI::Asset.new
-  a.key= "templates/404.liquid"
- 
-  a.src= "https://github.com/Shopify/Timber/blob/templates/404.liquid"
+  a.key= "templates/404.liquid" 
+  a.src= "https://github.com/sumanr1/suman_apps/blob/master/app_shop/app/views/new_file/newpage.liquid"
   a.save
 end
 end
